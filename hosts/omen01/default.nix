6f5ac36b0290bd275150d930598e9eaa635e534c
Caden Dinkel <cdink@Mac.home.local>
2026-06-26 21:33:26 -0500

    Moved nvidia out of config and added blacklist to nouveau.

--- hosts/omen01/default.nix
+++ hosts/omen01/default.nix
@@ -5,6 +5,14 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.blacklistedKernelModules = [ "nouveau" ];
+  hardware.graphics.enable = true;
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
   system.stateVersion = "26.05";
 }
