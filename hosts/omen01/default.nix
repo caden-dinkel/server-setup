b988f45b8ca9f2893e3d840450a4664a6801a4bc
Caden Dinkel <cdink@Mac.home.local>
2026-06-26 23:02:02 -0500

    changed to using production nvidia drivers.

--- hosts/omen01/default.nix
+++ hosts/omen01/default.nix
@@ -1,16 +1,30 @@
-{ pkgs, config, libs, ... }:
+{ pkgs, config, lib, ... }:
 {
   networking.hostName = "omen01";
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.blacklistedKernelModules = [ "nouveau" ];
+
   hardware.graphics.enable = true;
+
+  # Required to build and load the proprietary nvidia kernel module.
+  # modesetting = Intel iGPU; nvidia = discrete GPU (Optimus laptop).
+  services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
 
   hardware.nvidia = {
     modesetting.enable = true;
     open = false;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 
   system.stateVersion = "26.05";
